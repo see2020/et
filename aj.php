3ee0c7aded493c24b1fbca05b78767ca27c7d187
Denis <see_@mail.ru>
2021-11-02 14:56:48 +0500

    first commit

---
+++ aj.php
@@ -0,0 +1,27 @@
+<?php
+	header('Content-Type: text/html; charset=windows-1251;');
+	$cmsPathRelative = ".";
+	include($cmsPathRelative."/config.php");
+	$sql->sql_connect();
+	//aj.php?tbl=incident&id=6683&event=change&field=st&af=varbool
+	//./aj.php?af=spr.autocomplete&tbl_spr=&field_type=
+	
+	$TblNameDefault	 = (!isset($arrSetting['Table']['DefaultTable']) || $arrSetting['Table']['DefaultTable'] == "")?"":$arrSetting['Table']['DefaultTable'];
+	$TblName		 = (isset($_GET['tbl']))?trim($_GET['tbl']):$TblNameDefault;
+	$PageLink		 = "";
+	
+	$TblSetting = array();
+	include(GetIncFile($arrSetting,"inc.tables.config.set.php", ""));
+
+	function ajCheckAlias($alias = ""){
+		if($alias == ""){return($alias);}
+		$alias = strip_tags($alias);
+		$alias = preg_replace("/[^a-zA-Z0-9\_\.\s]/","",$alias);
+		$alias = preg_replace("/ {2,}/", " ", $alias);
+		$alias = trim($alias);
+		$alias = strtr($alias,array(" "=>"",));
+		return($alias);
+	}
+	include(GetIncFile($arrSetting,"inc.aj.".ajCheckAlias($_GET["af"]).".php", $TblSetting["table"]["name"]));
+	$sql->sql_close();
+?>
