1cdd4a2c24b0e6b1fccd854c0232a2ef6781c206
Denis <see_@mail.ru>
2021-11-02 15:16:36 +0500

    first commit

---
+++ components/tpl/default/list.php
@@ -0,0 +1,9 @@
+<?php
+	$return_var = "";
+	$return_var.= "<table class='tab_list'><thead>";
+	$return_var.= $arrTplData["row_head"];
+	$return_var.= "</thead><tbody>";
+	$return_var.= $arrTplData["row"];
+	$return_var.= "</tbody></table>";
+ ?>
+
