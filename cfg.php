997eaabebb13c2de4589e112dd098c97e1737759
Denis <seereserv@mail.ru>
2022-03-31 18:25:07 +0500

    поправка разделителя

--- cfg.php
+++ cfg.php
@@ -4,8 +4,8 @@
  */
 
 // разделитель
-define('DS', DIRECTORY_SEPARATOR);
-//define('DS', '/');
+//define('DS', DIRECTORY_SEPARATOR);
+define('DS', '/');
 // путь к корневой папке, D:\Server\domains\localhost\public_html\
 //define('ET_PATH_RELATIVE', realpath(dirname(__FILE__) . DS) . DS);
 // путь к корневой папке, D:\Server\domains\localhost\public_html
